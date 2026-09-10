4897877e0a401cef29a81a108b16d6bebc49e9ac
forstfe <grxq8hqb8r@privaterelay.appleid.com>
2026-09-10 10:07:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mealmate-rebuild-1';
-const ASSETS = ['./','index.html','styles.css?v=20260910','app.js?v=20260910','manifest.webmanifest','icons/icon-180.png','icons/icon-192.png','icons/icon-512.png'];
+const CACHE = 'mealmate-rebuild-2';
+const ASSETS = ['./','index.html','styles.css?v=20260910b','app.js?v=20260910b','manifest.webmanifest','icons/icon-180.png','icons/icon-192.png','icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
